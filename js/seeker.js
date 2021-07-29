ca1f64fdf1188fa455b014d9e3f66318a45803a9
davidrojom <davidrojo1997@gmail.com>
2021-07-29 02:35:59 +0200

    seeker refactor

--- js/seeker.js
+++ js/seeker.js
@@ -1,11 +1,10 @@
 export function seek(toSeek) {
 	let regExp = new RegExp(toSeek.toLowerCase());
-	const cards = document.getElementsByClassName("card");
-	for (let i = 0; i < cards.length; i++) {
-		if (!regExp.test(cards[i].getAttribute("id").toLowerCase())) {
-			cards[i].classList.add("hidden");
-		} else {
-			cards[i].classList.remove("hidden");
-		}
-	}
+	const cardCollection = document.getElementsByClassName("card");
+	const cards = Array.from(cardCollection);
+	cards.forEach((card) =>
+		regExp.test(card.getAttribute("id").toLowerCase())
+			? card.classList.remove("hidden")
+			: card.classList.add("hidden")
+	);
 }
